145aa1eb7ecb3f57ce71edc06f13f1e49781fbf1
Simon <simonski.dep@gmail.com>
2021-02-18 12:09:58 -0500

    initial setup

---
+++ src/table.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+
+const styles = theme => ({
+
+})
+
+class table extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        const { classes } = this.props;
+        return(
+            <Typography>This is the table component</Typography>
+
+        )
+    }
+}
+
+const Table = withStyles(styles)(table);
+
+export default Table;
